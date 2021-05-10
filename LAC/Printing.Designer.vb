3aa87dfd42ac5bb9b111ca6a1d390c01c84ccd78
santo-polibatam <susanto@polibatam.ac.id>
2021-05-10 21:55:20 +0800

    add QR Scan Fuji V.30

--- LAC/Printing.Designer.vb
+++ LAC/Printing.Designer.vb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Printing
     Inherits System.Windows.Forms.Form
@@ -50,56 +49,3 @@ Partial Class Printing
 
     Friend WithEvents ProgressBar1 As ProgressBar
 End Class
-=======
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Printing
-    Inherits System.Windows.Forms.Form
-
-    'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
-    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
-            End If
-        Finally
-            MyBase.Dispose(disposing)
-        End Try
-    End Sub
-
-    'Required by the Windows Form Designer
-    Private components As System.ComponentModel.IContainer
-
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
-    Private Sub InitializeComponent()
-        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
-        Me.SuspendLayout()
-        '
-        'ProgressBar1
-        '
-        Me.ProgressBar1.Location = New System.Drawing.Point(30, 34)
-        Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(250, 23)
-        Me.ProgressBar1.TabIndex = 0
-        '
-        'Printing
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(320, 88)
-        Me.ControlBox = False
-        Me.Controls.Add(Me.ProgressBar1)
-        Me.Name = "Printing"
-        Me.Opacity = 0.7R
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Printing"
-        Me.ResumeLayout(False)
-
-    End Sub
-
-    Friend WithEvents ProgressBar1 As ProgressBar
-End Class
->>>>>>> 5d2bc5751623f3413e8c53f892b2d6831328829d
--- LAC/Printing.vb
+++ LAC/Printing.vb
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 ﻿Public Class Printing
 
-=======
-﻿Public Class Printing
-
->>>>>>> 5d2bc5751623f3413e8c53f892b2d6831328829d
 End Class
